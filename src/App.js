f202e55ca29912807af50843c90a29ae6413f1a0
avila nicolas <aviladanielnicolas@gmail.com>
2023-01-15 19:57:06 -0300

    comienzo calculadora

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Boton from './componentes/boton'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cont-calculadora'>
+      <div className='fila'>
+        <Boton>1</Boton>
+        <Boton>+</Boton>
+      </div>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>
+      <div className='fila'></div>  
+    </div>
     </div>
   );
 }
